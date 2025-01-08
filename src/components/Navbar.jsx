fff49fb7280d16c3d9abca5f3a2f5c5827b25c3a
DeCode_Rimuru <vjuser25@gmail.com>
2025-01-08 13:30:08 +0800

    added new link for resume

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = () => {
   const [toggle, setToggle] = useState(false);
 
   const toggleResume = () => {
-    const resumeUrl = '/Resume.pdf';
+    const resumeUrl = 'vincent-porfolio/Resume.pdf';
     window.open(resumeUrl);
   };
 
